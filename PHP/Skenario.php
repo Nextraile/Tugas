bd147dd7d15ae7b4c7a8d90770c7b07f7899d3e6
Nextraile Vestrune <nextraile@gmail.com>
2025-08-08 22:39:41 +0700

    biar streaknya ga putus

--- PHP/Skenario.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once    "Parent Class.php";
-include_once    "Akun Reguler.php";
-include_once    "Akun Prioritas.php";
-
-$a = new akunPrioritas("Budi", 100000);
-echo $a->getInfoAkun() . "\n";
-echo $a->deposit(10000);
