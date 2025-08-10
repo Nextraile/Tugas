7c099ba44db908d0b17c607c6fbdf09a43b976c3
Nextraile Vestrune <nextraile@gmail.com>
2025-08-10 07:56:14 +0700

    Moved AkunBank, AkunPrioritas, and AkunReguler classes; Enhanced autoload function to search multiple directories for class files

--- PHP/OOP/init.php
+++ PHP/OOP/init.php
@@ -3,8 +3,32 @@
 // autoload function buat ngeload class secara otomatis
 spl_autoload_register(function ($class)
 {
-    $class = explode("/", $class);
-    $class = end($class);
-    $file = __DIR__ . "/" . $class . ".php";
-    require_once $file;
+    // ngereplace namespace jadi path yang bener
+    $class = str_replace("\\", "/", $class);
+
+    // $path yang berisi direktori tiap class
+    $path =
+    [
+        __DIR__ . '/models/',
+        __DIR__ . '/views/',
+        __DIR__ . '/controls/'
+    ];
+
+    // nyari file class di tiap direktori yang ada di $path
+    foreach ($path as $dir)
+    {
+        $file = $dir . $class . ".php";
+
+        // kalo file exists, require file tersebut
+        if (file_exists($file))
+        {
+            require_once $file;
+
+            // kalo ketemu, langsung return dan berhenti dari loop
+            return;
+        }
+    }
+
+    // kalo ga ketemu, lempar exception
+    throw new exception ("Class $class not found!");
 });
